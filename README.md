a0e815f0896549db66af85e220fb787a6a51a44d
RF <rayfdj@users.noreply.github.com>
2023-04-07 13:44:06 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# talk-to-a-doc
-Simple Python script to upload a document and then ask questions about it
+# Talk To A Doc(ument)
+
+This is a very simple Python script that asks you to pick a file, and then allows you to ask questions about the file. 
+
+It uses the great & powerful [LangChain](https://langchain.com/) to do the heavy lifting :muscle:.
+
+## Pre-requisites
+### OpenAI API Key
+You need to have an OpenAI API key. You can get one [here](https://platform.openai.com/account/api-keys).
+
+Set it as an environment variable called `OPENAI_API_KEY`.
+
+### Python & Dependencies
+I'm using Python 3.9, but [detectron2](https://github.com/facebookresearch/detectron2) works with Python ≥ 3.7, so any 
+version of Python 3.7 or higher should work.
+
+[pyenv](https://github.com/pyenv/pyenv) and the [pyenv-virtualenv](https://github.com/pyenv/pyenv-virtualenv) plugin are 
+highly recommended to keep your Python versions and environments cleanly separated.
+
+Once you have them installed and have the virtual environment activated, then install the following dependencies (the 
+below is for Mac):
+
+```bash
+brew install poppler
+brew install tesseract
+pip install langchain
+pip install "unstructured[local-inference]"
+pip install openai
+pip install chromadb
+pip install "detectron2@git+https://github.com/facebookresearch/detectron2.git@v0.6#egg=detectron2"
+```
+
+## Usage
+
+Simply run the script:
+
+```bash
+python main.py
+```
+
+And start asking questions! Ctrl + C to stop the session.
---
+++ main.py
@@ -0,0 +1,81 @@
+import os
+from tkinter import TclError
+from tkinter import Tk
+from tkinter import filedialog as fd
+
+from langchain import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import UnstructuredWordDocumentLoader, UnstructuredPDFLoader, \
+    UnstructuredEPubLoader, UnstructuredPowerPointLoader, UnstructuredFileLoader
+from langchain.indexes import VectorstoreIndexCreator
+
+
+def choose_a_doc() -> str:
+    root = Tk()
+    # credit to https://stackoverflow.com/a/54068050 for this trick
+    try:
+        try:
+            root.tk.call('tk_getOpenFile', '-foobarbaz')
+        except TclError:
+            pass
+        # now set the magic variables accordingly
+        root.tk.call('set', '::tk::dialog::file::showHiddenBtn', '1')
+        root.tk.call('set', '::tk::dialog::file::showHiddenVar', '0')
+    except TclError:
+        pass
+    root.withdraw()
+    return fd.askopenfilename(title="Select a document to talk to",
+                              initialdir=os.path.expanduser("~"),
+                              filetypes=(("PDF files", "*.pdf"), ("EPUB files", "*.epub"),
+                                         ("Word files", "*.docx *.doc"), ("PowerPoint files", "*.pptx *.ppt")))
+
+
+def select_loader_for_file(filename: str) -> UnstructuredFileLoader:
+    try:
+        loader = {
+            ".doc": lambda f: UnstructuredWordDocumentLoader(f),
+            ".docx": lambda f: UnstructuredWordDocumentLoader(f),
+            ".pdf": lambda f: UnstructuredPDFLoader(f),
+            ".epub": lambda f: UnstructuredEPubLoader(f),
+            ".ppt": lambda f: UnstructuredPowerPointLoader(f),
+            ".pptx": lambda f: UnstructuredPowerPointLoader(f),
+        }[os.path.splitext(filename)[1]](filename)
+        return loader
+    except KeyError:
+        print(f"File {filename} is of an unsupported file type.")
+        exit(1)
+
+
+def create_chain(loader: UnstructuredFileLoader) -> RetrievalQA:
+    index_creator = VectorstoreIndexCreator()
+    docsearch = index_creator.from_loaders([loader])
+
+    chain = RetrievalQA.from_chain_type(
+        llm=OpenAI(),
+        chain_type="stuff",
+        retriever=docsearch.vectorstore.as_retriever(),
+        input_key="question",
+    )
+    return chain
+
+
+def start_q_and_a(filename: str, chain: RetrievalQA):
+    print(f"You're asking questions about {os.path.split(filename)[1]}.")
+    print("Type your questions and press Enter. Press Ctrl + C to exit.")
+
+    while True:
+        try:
+            query = input(f"{os.linesep}Question: ")
+            sanitized_query = query.strip().replace(os.linesep, " ")
+            response = chain({"question": sanitized_query})
+            print(f"Answer: {response['result'].strip()}")
+        except KeyboardInterrupt:
+            print(f"{os.linesep}Goodbye!")
+            break
+
+
+if __name__ == "__main__":
+    document_filename = choose_a_doc()
+    document_loader = select_loader_for_file(document_filename)
+    qa_chain = create_chain(document_loader)
+    start_q_and_a(document_filename, qa_chain)
